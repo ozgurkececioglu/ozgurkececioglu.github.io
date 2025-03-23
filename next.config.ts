2e7e0635cf5ed5a7d826d17d7d1582ef628a099f
ozgurkececioglu <ozgur.kececioglu@gmail.com>
2025-03-23 22:51:13 +0300

    Add GHA for build and deploy the page

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+  // If your repository is named username.github.io, you don't need this
+  // If it's a custom repository name, you need to set the base path
+  // basePath: '/your-repo-name', // Remove this line if your repo is username.github.io
 };
 
 export default nextConfig;
